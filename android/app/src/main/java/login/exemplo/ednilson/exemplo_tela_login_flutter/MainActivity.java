be33a000e693db953fd3cee532a2f5e3a9e70631
ednilson <ednilsoncs@dcomp.ufs.br>
2019-07-02 08:27:44 -0300

    Adicionado arquivos do projeto

---
+++ android/app/src/main/java/login/exemplo/ednilson/exemplo_tela_login_flutter/MainActivity.java
@@ -0,0 +1,13 @@
+package login.exemplo.ednilson.exemplo_tela_login_flutter;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
